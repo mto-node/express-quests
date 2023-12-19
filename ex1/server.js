bea8a137d6ba0a5ebef01fee83435218cd60e12f
mToan <mto.work@outlook.com>
2023-12-19 09:57:05 +0100

    ex2

---
+++ ex1/server.js
@@ -0,0 +1,26 @@
+// Créer Serveur avec module HTTP
+const http = require("http");
+const port = 8000;
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  //   response.end("Hello Node.js Server!");
+  // Routage
+  if (request.url === "/") {
+    response.end("Hello Node.js Server!");
+  } else if (request.url === "/about") {
+    response.end("This demonstrates routing with Node.js.");
+  } else {
+    response.end("Default page (URLs other than / and /about)");
+  }
+};
+
+const server = http.createServer(requestHandler);
+server.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
+
